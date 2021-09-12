d5c797887324cb6d687bb08a382f1fc4fbe5802e
jstwinkles <35017895+jstwinkles@users.noreply.github.com>
2021-09-11 22:12:32 -0500

    spi_slave working in simulation

--- fw/sim/spi_slave_tb.vhd
+++ fw/sim/spi_slave_tb.vhd
@@ -33,14 +33,206 @@ architecture behavioral of spi_slave_tb is
   -- Constants --
   ---------------
 
+  constant c_clk_freq_hz        : natural := 50_000_000;
+  constant c_clk_half_period_ns : time := 10 ns;
+  constant c_sclk_period        : time := 1 us;
+  constant c_sclk_half_period   : time := 500 ns;
+
   -------------
   -- Signals --
   -------------
+
+  signal clk            : std_logic := '0';
+  signal rst            : std_logic := '0';
+  signal mosi           : std_logic := '0';
+  signal miso           : std_logic;
+  signal sclk           : std_logic := '1';
+  signal ss             : std_logic := '1';
+  signal done           : std_logic;
+  signal byte_to_send   : std_logic_vector(7 downto 0) := (others => '0');
+  signal received_byte  : std_logic_vector(7 downto 0);
 
   -------------
   -- Aliases --
   -------------
 
+  -- component spi_slave
+  -- port(
+  --   CLK   : in  std_logic;
+  --   RST   : in  std_logic;
+  --   SS    : in  std_logic;
+  --   MOSI  : in  std_logic;
+  --   MISO  : out std_logic;
+  --   SCK   : in  std_logic;
+  --   DONE  : out std_logic;
+  --   DIN   : in  std_logic_vector(7 downto 0);
+  --   DOUT  : out std_logic_vector(7 downto 0)
+  -- );
+  -- end component spi_slave;
+
 begin
+
+  -- Creates the system clock
+  clk_proc : process
+  begin
+    clk <= '0';
+    wait for c_clk_half_period_ns;
+    clk <= '1';
+    wait for c_clk_half_period_ns;
+  end process clk_proc;
+
+  -- Unit under test
+  uut_new : entity work.spi_slave
+  generic map(
+    G_CLK_FREQ_HZ => c_clk_freq_hz
+  )
+  port map(
+    CLK           => clk,
+    RST           => rst,
+    MOSI          => mosi,
+    MISO          => miso,
+    SCLK          => sclk,
+    SS            => ss,
+    DONE          => done,
+    BYTE_TO_SEND  => byte_to_send,
+    RECEIVED_BYTE => received_byte
+  );
+  -- uut_orig : spi_slave
+  -- port map(
+  --   CLK   => clk,
+  --   RST   => rst,
+  --   SS    => ss,
+  --   MOSI  => mosi,
+  --   MISO  => miso,
+  --   SCK   => sclk,
+  --   DONE  => done,
+  --   DIN   => byte_to_send,
+  --   DOUT  => received_byte
+  -- );
+
+  -- Acts as the SPI master
+  test_proc : process
+  begin
+    -- Wait an arbitrary delay
+    wait for c_sclk_period;
+
+    -- Slave should send 0xAA in the next transaction
+
+
+    -- Set SS low
+    byte_to_send <= x"AA";
+    ss <= '0';
+
+    -- Typical CS setup time
+    wait for 5 ns;
+
+    -- Send 8 bits to the slave (0xBB)
+    -- 7
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 6
+    mosi <= '0';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 5
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 4
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 3
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 2
+    mosi <= '0';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 1
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 0
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+
+    byte_to_send <= x"77";
+    -- Send 8 bits to the slave (0x47)
+    -- 7
+    mosi <= '0';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 6
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 5
+    mosi <= '0';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 4
+    mosi <= '0';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 3
+    mosi <= '0';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 2
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 1
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+    wait for c_sclk_half_period;
+    -- 0
+    mosi <= '1';
+    sclk <= '0';
+    wait for c_sclk_half_period;
+    sclk <= '1';
+
+    -- Typical CS hold time
+    wait for 8 ns;
+
+    -- Set SS high
+    ss <= '1';
+
+    wait;
+  end process test_proc;
 
 end behavioral;
--- fw/src/avr_interface/spi_slave.vhd
+++ fw/src/avr_interface/spi_slave.vhd
@@ -22,8 +22,19 @@ use ieee.std_logic_1164.all;
 
 entity spi_slave is
   generic(
+    G_CLK_FREQ_HZ : natural --! The system clock frequency in Hertz
   );
   port(
+    CLK           : in  std_logic; --! The system clock input signal
+    RST           : in  std_logic; --! The system reset input (active high)
+    --
+    MOSI          : in  std_logic; --! Master Out Slave In data signal
+    MISO          : out std_logic; --! Master In Slave Out data signal
+    SCLK          : in  std_logic; --! SPI clock signal
+    SS            : in  std_logic; --! Slave select signal
+    DONE          : out std_logic; --! Goes high for one clock cycle when a transaction has completed
+    BYTE_TO_SEND  : in  std_logic_vector(7 downto 0); --! The byte to send in the next transaction
+    RECEIVED_BYTE : out std_logic_vector(7 downto 0) --! The byte received in the latest transaction
   );
 end spi_slave;
 
@@ -33,6 +44,12 @@ architecture behavioral of spi_slave is
   -- Types --
   -----------
 
+  type state_t is (
+    st_idle,
+    st_active
+  );
+
+
   ---------------
   -- Constants --
   ---------------
@@ -41,10 +58,101 @@ architecture behavioral of spi_slave is
   -- Signals --
   -------------
 
+  -- The initial states here is to prevent accidentally detecting a non-existent edge on the registered signal
+  signal ss_d         : std_logic := '0';
+
+  signal sclk_d       : std_logic;
+  signal bit_counter  : natural range 0 to 7;
+  signal data_in      : std_logic_vector(7 downto 0);
+  signal data_out     : std_logic_vector(7 downto 0);
+
+  signal state        : state_t := st_idle;
+  signal byte_done    : std_logic := '0';
+
   -------------
   -- Aliases --
   -------------
 
 begin
+
+  -- This process implements a SPI slave device.  It first watches for a falling edge of the SS signal, indicating the
+  -- start of transactions.  As long as SS is low, data will continue to be shifted out of the byte that has been placed
+  -- on BYTE_TO_SEND.  After 8 bits have been sent, new data will be latched in from BYTE_TO_SEND and DONE will be
+  -- asserted high for one clock cycle.  This repeats until SS goes high.  MOSI is sampled on the rising edge of SCLK,
+  -- and MISO is updated on the falling edge of SCLK (SPI Mode 3).
+  process(CLK)
+  begin
+    if(rising_edge(CLK)) then
+      if(RST = '1') then
+
+      else
+        -- Register SCLK and SS for edge detection
+        sclk_d  <= SCLK;
+        ss_d    <= SS;
+
+        -- Default state so it's only pulsed high for one clock cycle
+        byte_done <= '0';
+        DONE      <= '0';
+        if(byte_done = '1') then
+          DONE          <= '1';
+          RECEIVED_BYTE <= data_in;
+        end if;
+
+        case(state) is
+          when st_idle =>
+            -- TODO: Hold MISO in high impedance while idle
+
+            -- Watch for falling edge of SS while idle
+            if(SS = '0' and ss_d = '1') then
+              -- On falling edge of SS, move to the active state, setup byte to send and first bit of MISO
+              state       <= st_active;
+              bit_counter <= 7;
+              MISO        <= BYTE_TO_SEND(7);
+              data_out    <= BYTE_TO_SEND;
+            end if;
+
+          when st_active =>
+            -- Continue in the active state as long as SS is held low
+            if(SS = '0') then
+
+              if(SCLK = '1' and sclk_d = '0') then
+                -- Sample MOSI on rising edge of SCLK
+                data_in(bit_counter) <= MOSI;
+
+                -- Move to the next bit after each rising edge
+                if(bit_counter = 0) then
+                  -- If we just sent/received the last bit, move to the next byte
+                  bit_counter <= 7;
+                  byte_done <= '1';
+                else
+                  bit_counter <= bit_counter - 1;
+                end if;
+              elsif(SCLK = '0' and sclk_d = '1') then
+                -- Setup MISO on falling edge of SCLK
+
+                if(bit_counter = 7) then
+                  data_out  <= BYTE_TO_SEND;
+                  MISO      <= BYTE_TO_SEND(7);
+                else
+                  MISO <= data_out(bit_counter);
+                end if;
+              end if;
+            else
+              -- If SS went high, go back to the idle state
+              state <= st_idle;
+
+              -- TODO: assert error if transaction was aborted mid-byte
+            end if;
+
+          when others =>
+            -- If we somehow get in a bad state, always go back to idle
+            state <= st_idle;
+        end case;
+
+
+      end if;
+    else
+    end if;
+  end process;
 
 end behavioral;
